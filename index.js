c1118ac09b84f705f90a578caa6dfa0242e1afb4
Emine Shaban <emineshaban@icloud.com>
2023-02-17 23:21:56 +0200

    created 2 sections and header

---
+++ index.js
@@ -0,0 +1,66 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("img");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+function onClick(n){
+    let p1 = document.getElementById('class1')
+    let p2 = document.getElementById('class2')
+    let p3 = document.getElementById('class3')
+
+    console.log(n)
+    console.log(p1)
+    // console.log(p2)
+    // console.log(p3)
+
+    if(n == 2){
+        p1.style.display = 'none'
+        p1.className = p3.className.replace(" click", "");
+
+        p3.style.display = 'none'
+        p3.className = p3.className.replace(" click", "");
+
+        p2.style.display = 'inline'
+        p2.className += ' click'
+
+    }else if(n == 3){
+        p1.style.display = 'none'
+        p1.className = p3.className.replace(" click", "");
+
+        p2.style.display = 'none'
+        p2.className = p3.className.replace(" click", "");
+
+        p3.style.display = 'inline'
+        p3.className += ' click'
+ 
+    }else if(n == 1){
+        p3.style.display = 'none'
+        p3.className = p3.className.replace(" click", "");
+
+        p2.style.display = 'none'
+        p2.className = p3.className.replace(" click", "");
+
+        p1.style.display = 'inline'
+        p1.className += ' click'
+ 
+    }
+}
